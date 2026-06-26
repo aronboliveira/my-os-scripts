713ef157f6e08945dd62207ee7374a2dd9d6dcee
Aron Barbosa de Oliveira <aron.b.96@gmail.com>
2026-06-26 12:16:39 -0300

    Add browser profile listing and incognito aliases

--- Bash/Linux/navigation_aliases.sh
+++ Bash/Linux/navigation_aliases.sh
@@ -4,6 +4,15 @@
     alias dl='cd ~/Downloads'
     alias ..='cd ..'
     alias ...='cd ../..'
-    alias .ilv='cd _inc/laravel'
+    alias brave='brave-browser'
+    alias brave-i="brave-browser --incognito"
+    alias brave-def='brave-browser --profile-directory="Default"'
+    alias brave-list-profiles='ls -la ~/.config/BraveSoftware/Brave-Browser | grep Profile'
+    alias ffx-list-profiles="cat ~/.mozilla/firefox/profiles.ini"
+    alias google-chrome-list-profiles="ls -la ~/.config/google-chrome | grep -E '^Default$|^Profile'"
+    alias ms-edge-list-profiles="ls -la ~/.config/microsoft-edge | grep -E '^Default$|^Profile'"
+    alias vivaldi-list-profiles="ls -la ~/.config/vivaldi | grep -E '^Default$|^Profile'"
+    alias ffx-i="firefox -private-window"
+    alias brave-tor='brave-browser --incognito --tor    
   #endregion Navigation_Aliases
 
